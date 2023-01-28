5ff6ae3876467e163792bc71d6c9b695082adbba
Sarahsegla <sarahsegla@icloud.com>
2023-01-28 20:05:31 +0000

    input text but might need to fix it up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 var currentDayEL = $('#currentDay');
 
 
-function displayTime() {
-    var now = moment().format('dddd');
-    currentDayEL.text(now);
-
-}
-
 console.log("hi")
 
 var currentDate = moment().format('dddd MMMM Do');
