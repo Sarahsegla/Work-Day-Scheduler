5020a9211321c4d24aa26d6a5cf67cc96fd01d74
Sarahsegla <sarahsegla@icloud.com>
2023-01-30 20:39:59 +0000

    changed submit to queryselector

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ var threePM = $('#3PM');
 var fourPM = $('#4PM');
 var fivePM = $('#5PM');
 // var middleIputField = $('#middle');
-var submitEl = $('#submit');
+//var submitEl = $('#submit');
+var submitEl = document.querySelector('#submit');
 
 console.log("hi")
  
@@ -46,18 +47,19 @@ $('#currentDay').text(currentDate);
 submitEl.addEventListener("click", function(event) {
     event.preventDefault();
 
-    var input = { 
-      nineam: nineAM.value.trim(),
-      tenam: tenAM.value.trim(),
-     elevenam: elevenAM.value.trim(),
-      twelvepm: twelvePM.value.trim(),
-      onepm: onePM.value.trim(),
-      twopm: twoPM.value.trim(),
-      threepm: threePM.value.trim(),
-      fourpm: fourPM.value.trim(),
-      fivepm: fivePM.value.trim(),
+    // var input = { 
+    //   nineam: nineAM.value.trim(),
+    //   tenam: tenAM.value.trim(),
+    //  elevenam: elevenAM.value.trim(),
+    //   twelvepm: twelvePM.value.trim(),
+    //   onepm: onePM.value.trim(),
+    //   twopm: twoPM.value.trim(),
+    //   threepm: threePM.value.trim(),
+    //   fourpm: fourPM.value.trim(),
+    //   fivepm: fivePM.value.trim(),
 
-    };
+    // };
+    
 
 
 
@@ -72,19 +74,29 @@ submitEl.addEventListener("click", function(event) {
 
 // if it passes a certain time it should change color.
 
-var now = new Date();
-var hours = now.getHours();
-var ft = now.toLocaleDateString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true
-});
+// var now = new Date();
+// var hours = now.getHours();
+// var ft = now.toLocaleDateString("en-US", {
+//     hour: "numeric",
+//     minute: "numeric",
+//     hour12: true
+// });
 
-if ( 5 <= hours && hours < 9) {
-nineAM.css('background', 'rgb(rgb(238, 232, 232)');
-var type = "9am";
-}
+// if ( 5 <= hours && hours < 9) {
+// nineAM.css('background', 'rgb(rgb(238, 232, 232)');
+// var type = "9am";
+// }
 
 
 
 //document.getElementById("9am").innerHTML = 'i dunno'
+
+// second attempt 
+
+var d = new Date().getHours();
+if (d < 10) {
+    document.write("fdfjjgbjrj");
+    document.body.style.backgroundColor="blue";
+} else if (d < 20) {
+    document.nineAM.style.backgroundColor="pink";
+}
