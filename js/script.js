f90a5d08452c163d6f824674db00e206da36f8fd
Sarahsegla <sarahsegla@icloud.com>
2023-01-30 16:28:11 +0000

    tried to get input text saved on java

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var currentDayEL = $('#currentDay');
 var inputTextEl = $('.input-text');
 var middleIputField = $('#middle');
+var submitEl = $('#submit');
 
 console.log("hi")
-
+ 
+// trying to save
 var currentDate = moment().format('dddd MMMM Do');
 $('#currentDay').text(currentDate);
 
@@ -15,6 +17,26 @@ function workDaySaved(event) {
    workDaySaved(input);
 
 }
+
+// second attempt to save
+function save() {
+var inputText = document.getElementById('input-text').value;
+localStorage.setItem('text', inputText);
+}
+
+// read data
+function get() {
+    var storeInput = localStorage.getItem('text');
+    if(storeInput) {
+        document.getElementById('input-text').value = storeInput;
+    }
+}
+
+
+
+
+
+
 // if it passes a certain time it should change color.
 
 
