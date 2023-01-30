aa1c6e05f9e7c6e9fb5e4abb0829ab897ede797d
Sarahsegla <sarahsegla@icloud.com>
2023-01-30 15:43:33 +0000

    added section to rows

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 var currentDayEL = $('#currentDay');
-
+var inputTextEl = $('.input-text');
+var middleIputField = $('#middle');
 
 console.log("hi")
 
 var currentDate = moment().format('dddd MMMM Do');
 $('#currentDay').text(currentDate);
+
+function workDaySaved(event) {
+    event.preventDefault();
+
+   var input = inputTextEl.val().trim();
+
+   workDaySaved(input);
+
+}
+// if it passes a certain time it should change color.
+
+
+
